b0c7e19d95c907fc8dcca15be993c41c4773cb89
Ahsan <ahsan.uet.ee.115@gmail.com>
2022-12-11 23:56:05 +0500

    Update tb of top file

--- sim/tb_riscv_pipelined_top.sv
+++ sim/tb_riscv_pipelined_top.sv
@@ -1,25 +1,27 @@
 //test bench for top file of riscv
 
 module tb_riscv_pipelined_top();
-   parameter DW = 32;
-   parameter REG_SIZE = 32;
+   parameter DW                  = 32;
+   parameter REG_SIZE            = 32;
    parameter NO_OF_REGS_REG_FILE = 32;
-   parameter REGW = $clog2(REG_SIZE);
-   parameter MEM_SIZE_IN_KB = 1;
-   parameter NO_OF_REGS = MEM_SIZE_IN_KB * 1024 / 4;
+   parameter REGW                = $clog2(REG_SIZE);
+   parameter MEM_SIZE_IN_KB      = 1;
+   parameter NO_OF_REGS          = MEM_SIZE_IN_KB * 1024 / 4;
    parameter ADDENT = 4;
+   parameter ADDRW               = 12;
 
    logic clk_i;
    logic rst_i;
 
 riscv_pipelined_top #(
-   .DW(DW),
-   .REG_SIZE(REG_SIZE),
+   .DW                 (DW                 ),
+   .REG_SIZE           (REG_SIZE           ),
    .NO_OF_REGS_REG_FILE(NO_OF_REGS_REG_FILE),
-   .REGW(REGW),
-   .MEM_SIZE_IN_KB(MEM_SIZE_IN_KB),
-   .NO_OF_REGS(NO_OF_REGS),
-   .ADDENT(ADDENT)
+   .REGW               (REGW               ),
+   .MEM_SIZE_IN_KB     (MEM_SIZE_IN_KB     ),
+   .NO_OF_REGS         (NO_OF_REGS         ),
+   .ADDENT             (ADDENT             ),
+   .ADDRW              (ADDRW              )
 
 )i_riscv_pipelined_top(
    .clk_i(clk_i),
